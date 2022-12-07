dd88d05b2a8513bbc9a11d3ba0bc0b2ecfe6fa65
Zyruks <kuzenshi@gmail.com>
2022-12-06 20:19:16 -0400

    build(Vite): :bricks: Modify Vite config and stylelint

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import postCSS from "postcss";
 import { qrcode } from "vite-plugin-qrcode";
-import { ViteTips } from "vite-plugin-tips";
+
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [postCSS(), qrcode(), ViteTips()],
+  plugins: [qrcode()],
 });
