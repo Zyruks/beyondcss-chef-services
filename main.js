18f1afaaa28fa672001c0cbe4eb1c3b4fed3caa3
Zyruks <kuzenshi@gmail.com>
2022-12-06 20:27:38 -0400

    feat(Js): :zap: Improve code to work with firefox
    Create a function that check if inside section-header live a P tag
    If header doesn’t have a p tag inside, add margin-block-end-36
    If header does have a p tag inside, add margin-block-end-90
    
    Then an if statement check is browser is not null && browser[0] is “Firefox”
    If these two are true, then run the function headerMargin()
    If not only console log Browser is not Firefox

---
+++ main.js
@@ -0,0 +1,27 @@
+"use strict";
+const browser = navigator.userAgent.match(/firefox|fxiso/i);
+const sectionHeader = document.querySelectorAll(".js-section-header");
+
+/**
+ * If the section header has a paragraph, add a margin of 36px to the bottom of the header. If the
+ * section header does not have a paragraph, add a margin of 90px to the bottom of the header
+ */
+function headerMargin() {
+  for (let header of sectionHeader) {
+    if (header.getElementsByTagName("p").length !== 0) {
+      header.classList.add("margin-block-end-36");
+      console.log("margin 36 working");
+    } else if (header.getElementsByTagName("p").length === 0) {
+      header.classList.add("margin-block-end-90");
+      console.log("margin 90 working");
+    }
+  }
+}
+
+/* This is checking to see if the browser is Firefox. If it is, it will run the headerMargin function.
+If it is not, it will log "Browser is working properly" to the console. */
+if (browser !== null && browser[0] === "Firefox") {
+  headerMargin();
+} else {
+  console.log("Browser is not Firefox");
+}
